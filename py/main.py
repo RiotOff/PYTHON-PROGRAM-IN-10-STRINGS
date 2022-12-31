dac76875fafbc97d7afa899612338ed47d2bace5
Райот (Riot) <daxkod3243@gmail.com>
2023-01-01 00:14:47 +0300

    Add files via upload

---
+++ py/main.py
@@ -0,0 +1,10 @@
+### Бесмысленная программа в 10 строчек кода (Вау!)
+
+from tkinter import *
+root = Tk()
+root.title('Десять строчек кода челлендж')
+root.geometry('500x500')
+root.resizable(width=False, height=False)
+click = Button(text='Всего 10 строчек...', width = '100', height = '100')
+click.pack()
+root.mainloop()
